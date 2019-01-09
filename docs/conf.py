e79dab497d02d0c1cb3efa7d27592cc961d038aa
mcflugen <mcflugen@gmail.com>
2019-01-09 09:51:12 -0700

    Add napoleon extension.

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ import sequence
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -153,3 +153,9 @@ texinfo_documents = [
         "Miscellaneous",
     )
 ]
+
+napoleon_numpy_docstring = True
+napoleon_google_docstring = False
+napoleon_include_init_with_doc = True
+napoleon_include_special_with_doc = True
+
