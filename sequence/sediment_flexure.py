d14af5c34c8af96847738dc900f1b1b17d44c84e
Michael S. Steckler <michael@dhcp-22-142.ldeo.columbia.edu>
2019-05-10 12:38:39 -0400

    corrected flexure so calculate load densities using lithology and include submarine load displacing water

--- sequence/sediment_flexure.py
+++ sequence/sediment_flexure.py
@@ -1,6 +1,7 @@
 from landlab.components.flexure import Flexure1D
 import numpy as np
 
+
 class SedimentFlexure(Flexure1D):
 
     _name = "Sediment-loading flexure"
@@ -30,35 +31,63 @@ class SedimentFlexure(Flexure1D):
         "bedrock_surface__elevation": "New bedrock elevation following subsidence",
     }
 
-    def __init__(self, grid, rho_sediment=1600.0, isostasytime=7000.0, **kwds):
-        self._rho_sediment = rho_sediment
+    def __init__(
+        self,
+        grid,
+        sand_density=2650,
+        mud_density=2720.0,
+        isostasytime=7000.0,
+        **kwds,
+        # **sediments,
+    ):
+        self._rho_sand = sand_density * (1 - 0.4) + 1030.0 * 0.4  # porosity = 40%
+        self._rho_mud = mud_density * (1 - 0.65) + 1030.0 * 0.65  # porosity = 65%
         self._isostasytime = isostasytime
-        self._dt = 100.  #default timestep = 100 y
-      
+        self._dt = 100.0  # default timestep = 100 y
+
         Flexure1D.__init__(self, grid, **kwds)
 
         self.grid.add_zeros("lithosphere__increment_of_overlying_pressure", at="node")
         self.grid.add_zeros("lithosphere_surface__increment_of_elevation", at="node")
-        self.subs_pool = self.grid.add_zeros('node','subsidence_pool')
-
+        self.subs_pool = self.grid.add_zeros("node", "subsidence_pool")
 
     @property
-    def rho_sediment(self):
-        return self._rho_sediment
+    def sand_density(self):
+        return self._rho_sand
+
+    @property
+    def mud_density(self):
+        return self._rho_mud
 
     def update(self):
 
-        if self._isostasytime > 0.:
-            isostasyfrac = (1 - np.exp(-1.* self._dt/self._isostasytime))
+        if self._isostasytime > 0.0:
+            isostasyfrac = 1 - np.exp(-1.0 * self._dt / self._isostasytime)
         else:
-            isostasyfrac = 1.
+            isostasyfrac = 1.0
 
         self.grid.at_node["lithosphere_surface__increment_of_elevation"][:] = 0.0
 
-        dz = self.grid.at_node["sediment_deposit__thickness"]
-        self.grid.at_node["lithosphere__increment_of_overlying_pressure"][:] = (
-            dz * self.rho_sediment * self.gravity * self.grid.dx
+        # calculate density based on sand_frac
+        percent_sand = self.grid.at_node[
+            "delta_sediment_sand__volume_fraction"
+        ].reshape(self.grid.shape)[1]
+        rho_sediment = (
+            percent_sand * self._rho_sand + (1 - percent_sand) * self._rho_mud
         )
+
+        # load underwater displaces water
+        z = self.grid.at_node["topographic__elevation"].reshape(self.grid.shape)[1]
+        under_water = z < 0.0
+        rho_sediment[under_water] = rho_sediment[under_water] - 1030.0
+
+        dz = self.grid.at_node["sediment_deposit__thickness"].reshape(self.grid.shape)[
+            1
+        ]
+        pressure = self.grid.at_node[
+            "lithosphere__increment_of_overlying_pressure"
+        ].reshape(self.grid.shape)[1]
+        pressure[:] = dz * rho_sediment * self.gravity * self.grid.dx
 
         Flexure1D.update(self)
 
@@ -66,13 +95,12 @@ class SedimentFlexure(Flexure1D):
         self.subs_pool[:] += dz
         dz = self.subs_pool[:] * isostasyfrac
         self.subs_pool[:] = self.subs_pool[:] - dz
-        
-	
+
         self.grid.at_node["bedrock_surface__increment_of_elevation"][:] = dz
 
         self.grid.at_node["bedrock_surface__elevation"] -= dz
         self.grid.at_node["topographic__elevation"] -= dz
 
-    def run_one_step(self, dt=100.):
+    def run_one_step(self, dt=100.0):
         self._dt = dt
         self.update()
