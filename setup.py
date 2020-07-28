2e7f782dfb2dbbc55b4c222e7a4a57e059ada277
mcflugen <mcflugen@gmail.com>
2020-07-28 16:51:40 -0600

    remove versioneer; use zest.releaser

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import find_packages, setup
 
-import versioneer
-
 
 def read(filename):
     with open(filename, "r", encoding="utf-8") as fp:
@@ -18,6 +16,8 @@ long_description = u'\n\n'.join(
 
 
 setup(
+    name="sequence",
+    version="0.1.2",
     author="Eric Hutton",
     author_email="mcflugen@gmail.com",
     classifiers=[
@@ -30,18 +30,15 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    description="Python version of the Steckler Sequence model",
+    description="Python version of the Steckler Sequence model built with landlab",
     long_description=long_description,
     install_requires=open("requirements.txt", "r").read().splitlines(),
     license="MIT license",
     include_package_data=True,
-    keywords=["sequence", "landlab"],
-    name="sequence",
+    keywords=["sequence", "stratigraphy", "landlab"],
     packages=find_packages(include=["sequence"]),
     test_suite="tests",
     url="https://github.com/sequence-dev/sequence",
-    version=versioneer.get_version(),
     entry_points={"console_scripts": ["sequence=sequence.cli:sequence"]},
-    cmdclass=versioneer.get_cmdclass(),
     zip_safe=False,
 )
