533ba42fb3271b059595c5d787f2e66ff1767c25
mcflugen <mcflugen@gmail.com>
2020-07-30 14:40:07 -0600

    add tests for dumping config files

--- tests/test_time_varying_config.py
+++ tests/test_time_varying_config.py
@@ -17,13 +17,13 @@ def test_from_file_basic(datadir, fmt):
     assert config.as_dict() == {"foo": {"bar": "baz"}, "constant": {"pi": 3.14}}
 
 
-@pytest.mark.parametrize("src_fmt", ("yaml", "toml"))
 @pytest.mark.parametrize("dst_fmt", ("yaml", "toml"))
-def test_dump(tmpdir, datadir, src_fmt, dst_fmt):
+@pytest.mark.parametrize("src_fmt", ("yaml", "toml"))
+@pytest.mark.parametrize("basename", ("config", "config-basic"))
+def test_dump(tmpdir, datadir, basename, src_fmt, dst_fmt):
     with tmpdir.as_cwd():
-        config = TimeVaryingConfig.from_file(datadir / f"config.{src_fmt}")
+        config = TimeVaryingConfig.from_file(datadir / f"{basename}.{src_fmt}")
         with open(f"config.{dst_fmt}", "w") as fp:
             fp.write(config.dump(fmt=dst_fmt))
         actual = TimeVaryingConfig.from_file(f"config.{dst_fmt}")
         assert actual.as_dict() == config.as_dict()
-
