f3e056d07c54df3958297c8eb0b3dbab937e5137
mcflugen <mcflugen@gmail.com>
2020-08-11 16:32:00 -0600

    add tests for SubmarineDiffuser setters

---
+++ tests/test_submarine_diffuser.py
@@ -0,0 +1,41 @@
+from random import random
+
+import pytest
+from numpy.testing import assert_array_equal
+
+from sequence.submarine import SubmarineDiffuser
+from landlab import RasterModelGrid
+
+
+def test_diffuser():
+    grid = RasterModelGrid((3, 5))
+    grid.add_zeros("topographic__elevation", at="node")
+    grid.at_grid["sea_level__elevation"] = 0.0
+
+    diffuser = SubmarineDiffuser(grid)
+
+
+@pytest.mark.parametrize(
+    "param",
+    (
+        "plain_slope",
+        "wave_base",
+        "shoreface_height",
+        "alpha",
+        "shelf_slope",
+        "sediment_load",
+    ),
+)
+def test_setters(param):
+    grid = RasterModelGrid((3, 5))
+    grid.add_zeros("topographic__elevation", at="node")
+    grid.at_grid["sea_level__elevation"] = 0.0
+
+    expected = random()
+    diffuser = SubmarineDiffuser(grid, **{param: expected})
+
+    assert getattr(diffuser, param) == pytest.approx(expected)
+
+    expected = random()
+    setattr(diffuser, param, expected)
+    assert getattr(diffuser, param) == pytest.approx(expected)
