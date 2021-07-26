ef8d80fb01397867b8ed8d63af4e2378df6f435b
mcflugen <mcflugen@gmail.com>
2021-07-26 16:20:42 -0600

    skip failing test on linux

--- tests/test_sequence_model.py
+++ tests/test_sequence_model.py
@@ -1,17 +1,21 @@
 import os
 import shutil
 
+import pytest
 from landlab.core import load_params
 
 from sequence.sequence_model import SequenceModel
 
 
-# def test_marmara(tmpdir, datadir):
-#     with tmpdir.as_cwd():
-#         for fname in datadir.iterdir():
-#             shutil.copy(str(datadir / fname), ".")
-#         params = load_params("marmara.yaml")
-#         model = SequenceModel(**params)
-#         model.run()
+@pytest.mark.skipif(
+    os.platform.startswith("linux"), reason="possible problem with Ubuntu"
+)
+def test_marmara(tmpdir, datadir):
+    with tmpdir.as_cwd():
+        for fname in datadir.iterdir():
+            shutil.copy(str(datadir / fname), ".")
+        params = load_params("marmara.yaml")
+        model = SequenceModel(**params)
+        model.run()
 
-#         assert os.path.isfile("marmara.nc")
+        assert os.path.isfile("marmara.nc")
