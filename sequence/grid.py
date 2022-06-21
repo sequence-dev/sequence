5488e5c01414c7a6fa08ca60b6e196ad62275598
mcflugen <mcflugen@gmail.com>
2022-06-21 13:03:56 -0600

    add SequenceModelGrid class

---
+++ sequence/grid.py
@@ -0,0 +1,9 @@
+from landlab import RasterModelGrid
+
+
+class SequenceModelGrid(RasterModelGrid):
+    def __init__(self, n_cols, spacing=100.0):
+        super().__init__((3, n_cols), xy_spacing=spacing)
+
+        self.status_at_node[self.nodes_at_top_edge] = self.BC_NODE_IS_CLOSED
+        self.status_at_node[self.nodes_at_bottom_edge] = self.BC_NODE_IS_CLOSED
