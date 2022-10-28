438a25e8534638fffe2382f1f6656726fddb1826
mcflugen <mcflugen@gmail.com>
2022-10-28 09:34:11 -0600

    remove process components from top-level import

--- sequence/__init__.py
+++ sequence/__init__.py
@@ -1,15 +1,11 @@
 from ._version import __version__
 from .grid import SequenceModelGrid
-from .output_writer import OutputWriter
 from .sequence import Sequence
-from .submarine import SubmarineDiffuser
+from .sequence_model import SequenceModel
 
 __all__ = [
     "__version__",
-    "OutputWriter",
     "Sequence",
-    "SubmarineDiffuser",
-    "SeaLevelTimeSeries",
+    "SequenceModel",
     "SequenceModelGrid",
-    "SinusoidalSeaLevel",
 ]
