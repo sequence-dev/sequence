217b79506993be9cd3e1eb72ad00ce17814c19a3
mcflugen <mcflugen@gmail.com>
2022-11-02 10:53:53 -0600

    add bedrock elevation field if it's not already there

--- sequence/subsidence.py
+++ sequence/subsidence.py
@@ -1,10 +1,17 @@
-#! /usr/bin/env python
+"""
+Subside a grid
+==============
+"""
+import os
+
 import numpy as np
 from landlab import Component
 from scipy import interpolate
 
 
 class SubsidenceTimeSeries(Component):
+
+    """A *Landlab* component that subsides a grid."""
 
     _name = "Subsider"
 
@@ -29,20 +36,23 @@ class SubsidenceTimeSeries(Component):
         },
     }
 
-    def __init__(self, grid, filepath=None, kind="linear"):
-        """Generate subsidence rates.
+    def __init__(self, grid, filepath: os.PathLike, kind: str = "linear"):
+        """Create a grid subsider from a time-series file.
 
         Parameters
         ----------
-        grid: RasterModelGrid
+        grid: :class:`~sequence.grid.SequenceModelGrid`
             A landlab grid.
-        filepath: str
+        filepath: os.PathLike
             Name of csv-formatted subsidence file.
         kind: str, optional
             Kind of interpolation as a string (one of 'linear',
             'nearest', 'zero', 'slinear', 'quadratic', 'cubic').
             Default is 'linear'.
         """
+        if "bedrock_surface__elevation" not in grid.at_node:
+            grid.add_empty("bedrock_surface__elevation", at="node")
+
         super().__init__(grid)
 
         self._filepath = filepath
@@ -52,9 +62,12 @@ class SubsidenceTimeSeries(Component):
         subsidence = SubsidenceTimeSeries._subsidence_interpolator(
             data, kind=self._kind
         )
-        inc = self.grid.add_empty(
-            "bedrock_surface__increment_of_elevation", at="node"
-        ).reshape(self.grid.shape)
+        if "bedrock_surface__increment_of_elevation" not in grid.at_node:
+            self.grid.add_empty("bedrock_surface__increment_of_elevation", at="node")
+
+        inc = self.grid.at_node["bedrock_surface__increment_of_elevation"].reshape(
+            self.grid.shape
+        )
         inc[:] = subsidence(self.grid.x_of_node[self.grid.nodes_at_bottom_edge])
 
         self._dz = inc.copy()
@@ -72,15 +85,17 @@ class SubsidenceTimeSeries(Component):
         )
 
     @property
-    def time(self):
+    def time(self) -> float:
+        """Return the current component time."""
         return self._time
 
     @property
-    def filepath(self):
-        return self._filepath
+    def filepath(self) -> str:
+        """Return the path to the current subsidence file."""
+        return str(self._filepath)
 
     @filepath.setter
-    def filepath(self, new_path):
+    def filepath(self, new_path: os.PathLike):
         self._filepath = new_path
         subsidence = SubsidenceTimeSeries._subsidence_interpolator(
             np.loadtxt(self._filepath, delimiter=",", comments="#"), kind=self._kind
@@ -91,7 +106,14 @@ class SubsidenceTimeSeries(Component):
         inc[:] = subsidence(self.grid.x_of_node[self.grid.nodes_at_bottom_edge])
         self._dz = inc.copy()
 
-    def run_one_step(self, dt):
+    def run_one_step(self, dt: float) -> None:
+        """Update the component by a time step.
+
+        Parameters
+        ----------
+        dt : float
+            The time step to update the component by.
+        """
         dz = self.grid.at_node["bedrock_surface__increment_of_elevation"]
         z = self.grid.at_node["bedrock_surface__elevation"]
         z_top = self.grid.at_node["topographic__elevation"]
