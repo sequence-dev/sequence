d0ca8d2aebe652b254292ca43d57ca8e8ceb47d1
mcflugen <mcflugen@gmail.com>
2023-10-21 11:45:47 -0600

    add some errors for invalid output files

--- sequence/errors.py
+++ sequence/errors.py
@@ -29,15 +29,33 @@ class ShelfEdgeError(SequenceError):
         return self._msg
 
 
-class MissingRequiredVariable(SequenceError):
+class OutputValidationError(SequenceError):
+    """Raise this exception if there is something wrong with an output file."""
+
+    pass
+
+
+class InvalidRowError(OutputValidationError):
+    """Raise this exception if a required variable is missing from an output file."""
+
+    def __init__(self, row: int, n_rows: int):
+        self._row = row
+        self._n_rows = n_rows
+
+    def __str__(self) -> str:
+        """Return error message that includes the requested row."""
+        return f"row {self._row!r} is out of bounds for output file with {self._n_rows} rows"
+
+
+class MissingRequiredVariable(OutputValidationError):
     """Raise this exception if a required variable is missing from an output file."""
 
     def __init__(self, name: str):
         self._name = name
 
-    def __str_(self) -> str:
+    def __str__(self) -> str:
         """Return error message that includes the name of the missing variable."""
-        return f"{self._name!r}"
+        return f"requireed variable ({self._name!r}) is missing from output file"
 
 
 class ParameterMismatchError(SequenceError):
