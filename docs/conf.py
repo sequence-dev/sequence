7bbfb50e94af85379fa7016297a82bbcd96c0e99
mcflugen <mcflugen@gmail.com>
2023-12-20 20:56:50 -0700

    add a sequence logo to the docs

--- docs/conf.py
+++ docs/conf.py
@@ -114,6 +114,7 @@ todo_include_todos = False
 # html_theme = "alabaster"
 html_theme = "furo"
 html_title = "Sequence"
+html_logo = "_static/sequence-logo.png"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
